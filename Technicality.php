9775a244d116e4474d3478aaa9d8a39ffba63b4c
Jeff Trotman <jeff@jefftrotman.com>
2019-06-09 22:40:46 -0500

    Adding gaTrackingId setting

--- Technicality.php
+++ Technicality.php
@@ -7,6 +7,9 @@
  */
  
 function hook_gaheader() {
+    $setting = get_option('technicality_gaTrackingId');
+    if (isset( $setting ))
+    {
     ?>
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141740265-2"></script>
@@ -15,8 +18,45 @@ function hook_gaheader() {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'UA-141740265-2');
+            //gtag('config', 'UA-141740265-2');
+            gtag('config', <?php esc_attr( $setting ) ?>)
         </script>
     <?php
+    }
 }
 add_action('wp_head', 'hook_gaheader');
+
+function technicality_settings_init()
+{
+    // register a new setting for "reading" page
+    register_setting('reading', 'technicality_gaTrackingId');
+ 
+    // register a new field in the "wporg_settings_section" section, inside the "reading" page
+    add_settings_field(
+        'technicality_gaTrackingId_field',
+        'Google Analytics Tracking ID',
+        'technicality_settings_field_cb',
+        'reading'
+        'default'
+    );
+}
+ 
+/**
+ * register technicality_settings_init to the admin_init action hook
+ */
+add_action('admin_init', 'technicality_settings_init');
+ 
+/**
+ * callback functions
+ */
+ 
+// field content cb
+function technicality_settings_field_cb()
+{
+    // get the value of the setting we've registered with register_setting()
+    $setting = get_option('technicality_gaTrackingId');
+    // output the field
+    ?>
+    <input type="text" name="technicality_gaTrackingId" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+    <?php
+}
