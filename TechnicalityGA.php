6f02ae1bdd1f8236048d6ede5d2c54197100da2c
Jeff Trotman <jeff@jefftrotman.com>
2019-06-10 20:58:31 -0500

    Changed version and author

--- TechnicalityGA.php
+++ TechnicalityGA.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name: Technicality Google Analytics
  * Plugin URI: http://technicality.online/products/GoogleAnalyticsWordPressPlugin
- * Version: 20190610
+ * Version: 1.0.0
  * Description: Simple - enter your Google Tracking ID in a new field on the General Settings page and the Google Analytics script will be added to the header of each post and page.
- * Author: Technicality LLC
+ * Author: Jeff Trotman, Technicality LLC
  * Author URI: http://technicality.online
  */
  
